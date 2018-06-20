e0fe7512a0c1f820ce5cebea288d1d9ff2afd5b6
Aron Davies <aron.d28@gmail.com>
2018-06-20 11:57:33 +0100

    Re-ordered import statements in the app.module file to keep the angular ones together at the top, and ones closely linked together next to each other.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CharacterService } from './services/character.service';
 import { CharactersComponent } from './characters/characters.component';
+import { CharacterInfoComponent } from './character-info/character-info.component';
+import { MapService } from './services/map.service';
 import { MapsComponent } from './maps/maps.component';
+import { MapInfoComponent } from './map-info/map-info.component';
 import { PlayerStatsComponent } from './player-stats/player-stats.component';
 import { ErrorComponent } from './error/error.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { CharacterInfoComponent } from './character-info/character-info.component';
-import { CharacterService } from './services/character.service';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MapService } from './services/map.service';
-import { MapInfoComponent } from './map-info/map-info.component';
 
 @NgModule({
     declarations: [
